f833ad824c836ab25a9cc9945de7f9b730e86068
Brooke <brookekelseyryan@gmail.com>
2020-12-07 15:17:09 -0800

    Added Jillian's board.py file.

---
+++ board.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class Board:
+    _characters = {
+        ' ': 0,  # free space
+        '@': 1,  # player
+        '#': 2,  # wall
+        '$': 3,  # box
+        '.': 4,  # goal
+    }
+
+    def __init__(self, filename):
+
+        with open(filename, 'r') as f:
+
+            read_data = f.read()
+            self._lines = read_data.split("\n")
+
+            self.width = max(list(map(lambda x: len(self._lines[x]), range(len(self._lines)))))
+            self.height = len(self._lines)
+
+            self.board = np.zeros((self.width, self.height), dtype=int)
+
+            for i in range(len(self._lines)):
+                line = self._lines[i]
+                for j, character in enumerate(line):
+                    self.board[j][i] = self._characters[character]
