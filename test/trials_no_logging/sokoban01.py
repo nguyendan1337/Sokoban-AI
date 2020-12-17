de7ee16c22ffd9700b5e566f2147ff5580555f4a
Brooke <brookekelseyryan@gmail.com>
2020-12-16 21:58:57 -0800

    Added trial runs for all of Kask's benchmark inputs.

--- test/trials_no_logging/sokoban01.py
+++ test/trials_no_logging/sokoban01.py
@@ -7,11 +7,3 @@ Trial(file="../input/kask_input/sokoban01.txt",
       learning_rate=0.9,
       r=5000,
       logging=False).run()
-
-Trial(file="../input/kask_input/sokoban01.txt",
-      mode="kask",
-      epsilon=0.8,
-      discount_factor=0.9,
-      learning_rate=0.9,
-      r=5000,
-      logging=True).run()
