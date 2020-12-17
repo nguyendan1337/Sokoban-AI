7f13194e700b8507fdca5c19a2a0f28bd774d2f8
Brooke <brookekelseyryan@gmail.com>
2020-12-17 00:34:54 -0800

    Parameterized testing to run overnight.

--- output_format.py
+++ output_format.py
@@ -1,11 +1,9 @@
-
 def output(s):
     moves = len(s)
-    s=s+['end']
-    # print(s)
+    s = s + ['end']
     i = 0
-    series=""
-    while (i < len(s) - 1):
+    series = ""
+    while i < len(s) - 1:
         # Counting occurrences of s[i]
         count = 1
         while s[i][0] == s[i + 1][0]:
@@ -13,6 +11,7 @@ def output(s):
             count += 1
             if i + 1 == len(s):
                 break
-        series=series+(str(count)+str(s[i][0]))
+        series = series + (str(count) + str(s[i][0]))
         i += 1
-    print(moves,'moves',series) #prints number of moves and series of moves
+    print(moves, 'moves', series)  # prints number of moves and series of moves
+    return series
